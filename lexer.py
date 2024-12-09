d4b025671231e151610141231f297079eae0d16c
Izabel Nascimento <izabelyale@hotmail.com>
2024-12-08 22:33:45 -0300

    adding error line

--- lexer.py
+++ lexer.py
@@ -3,7 +3,6 @@ import re
 
 class Lexer:
     def __init__(self):
-        # Define os padrões de tokens
         self.tokens = [
             ('NUMBER', r'\d+'),
             ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*'),
@@ -19,16 +18,19 @@ class Lexer:
 
     def tokenize(self, code):
         position = 0
+        line_number = 1
         while position < len(code):
             match = None
             for token_type, pattern in self.tokens:
                 regex = re.compile(pattern)
                 match = regex.match(code, position)
                 if match:
-                    if token_type != 'WHITESPACE':  # Ignora espaços
-                        yield (token_type, match.group(0))
+                    if token_type == 'WHITESPACE':
+                        line_number += match.group(0).count('\n')
+                    else:
+                        yield (token_type, match.group(0), line_number)
                     position = match.end()
                     break
             if not match:
-                raise SyntaxError(f"Unexpected character: {code[position]}")
+                raise SyntaxError(f"Unexpected character '{code[position]}' on line {line_number}")
         print("Análise Léxica concluída sem erros")
