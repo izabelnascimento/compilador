bba7ac99a585158e672fe868599df9490ad3dbe0
leonardonb <leonardonb@gmail.com>
2024-12-10 02:33:21 -0300

    ADD Adição de arquivos com códigos de teste, reorganização de arquivos e acrescimos em lexer.py

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from src.compiler import Compiler
 
 def main():
     compiler = Compiler()
-    for i in range(1, 6):
-        file_path = f'../resources/code{i}_1.txt'  #Nos exemplos do 1 a 4 são acertos, do 5 ao 10 são erros, mas t´não tá se comportando assim. Precisa ser revisto
+    for i in range(1, 11):
+        file_path = f'../resources/test_lexer/code{i}_1.txt'
         compiler.compile(file_path)
-        file_path = f'../resources/code{i}_2.txt'  #Nos exemplos do 1 a 4 são acertos, do 5 ao 10 são erros, mas t´não tá se comportando assim. Precisa ser revisto
+        file_path = f'../resources/test_lexer/code{i}_2.txt'
         compiler.compile(file_path)
 
 
