4cd8557b3894aa1ce57532d5d6ff3c85dfab6f04
Izabel Nascimento <izabelyale@hotmail.com>
2024-12-09 22:08:53 -0300

    implementado compiler.py e loop de execução dos testes

---
+++ src/compiler.py
@@ -0,0 +1,48 @@
+import sys
+
+from src.lexer import Lexer
+from src.syntax import Syntax
+
+
+class Compiler:
+
+    def compile(self, file_path):
+        code = self.read_code_from_file(file_path)
+        if not code:
+            return
+
+        print(f"\n-------------- Compilando arquivo {file_path} -------------- ")
+        try:
+            lexer = Lexer()
+            tokens = list(lexer.tokenize(code))
+            print("\nTokens:", tokens)
+            self.print_sucess("Análise Léxica concluída sem erros")
+            try:
+                syntax = Syntax(tokens)
+                # syntax.parse()
+                # self.print_sucess("\nAnálise sintática concluída sem erros.")
+            except SyntaxError as e:
+                self.print_error(f"\nErro durante a análise sintática: {e}")
+        except SyntaxError as e:
+            self.print_error(f"\nErro durante a análise léxica: {e}")
+
+    @staticmethod
+    def read_code_from_file(file_path):
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                code = file.read()
+            return code
+        except FileNotFoundError:
+            print(f"Erro: O arquivo '{file_path}' não foi encontrado.")
+            return ""
+        except Exception as e:
+            print(f"Erro ao ler o arquivo: {e}")
+            return ""
+
+    @staticmethod
+    def print_error(texto):
+        print(f"\033[31m{texto}\033[0m")
+
+    @staticmethod
+    def print_sucess(mensagem):
+        sys.stdout.write(f"\033[32m{mensagem}\033[0m\n")
--- src/main.py
+++ src/main.py
@@ -1,38 +1,11 @@
-from lexer import Lexer
-from syntax import Syntax
+from src.compiler import Compiler
 
 
 def main():
-    file_path = '../resources/code05_2.txt'  #Nos exemplos do 1 a 4 são acertos, do 5 ao 10 são erros, mas t´não tá se comportando assim. Precisa ser revisto
-    code = read_code_from_file(file_path)
-    if not code:
-        return
-
-    try:
-        lexer = Lexer()
-        tokens = list(lexer.tokenize(code))
-        print("\nTokens:", tokens)
-        try:
-            syntax = Syntax(tokens)
-            syntax.parse()
-            print("\nAnálise sintática concluída sem erros.")
-        except SyntaxError as e:
-            print(f"\nErro durante a análise sintática: {e}")
-    except SyntaxError as e:
-        print(f"\nErro durante a análise léxica: {e}")
-
-
-def read_code_from_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            code = file.read()
-        return code
-    except FileNotFoundError:
-        print(f"Erro: O arquivo '{file_path}' não foi encontrado.")
-        return ""
-    except Exception as e:
-        print(f"Erro ao ler o arquivo: {e}")
-        return ""
+    compiler = Compiler()
+    for i in range(1, 11):
+        file_path = f'../resources/code{i}.txt'  #Nos exemplos do 1 a 4 são acertos, do 5 ao 10 são erros, mas t´não tá se comportando assim. Precisa ser revisto
+        compiler.compile(file_path)
 
 
 if __name__ == "__main__":
