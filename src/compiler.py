665c3c7fe0d179dddc5eb0a1b1060ccec77e8d12
Izabel Nascimento <izabelyale@hotmail.com>
2024-12-11 00:57:04 -0300

    salvando tabelas em csv

--- src/compiler.py
+++ src/compiler.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 
 from src.lexer import Lexer
@@ -7,18 +8,20 @@ from src.syntax import Syntax
 
 class Compiler:
 
-    def compile(self, file_path):
-        code = self.read_code_from_file(file_path)
+    def compile(self, file_name):
+        code = self.read_code_from_file(f'../resources/test_lexer/{file_name}')
         if not code:
             return
 
-        print(f"\n-------------- Compilando arquivo {file_path} -------------- ")
+        print(f"\n-------------- Compilando arquivo {file_name} -------------- ")
         try:
             lexer = Lexer()
             tokens = list(lexer.tokenize(code))
             print("\nTokens:", tokens)
+            self.save_tokens_to_csv(tokens, file_name)
             symbol_table = Symbol.symbol_table(tokens)
             print("\nSymbol Table:", symbol_table)
+            self.save_symbol_table_to_csv(symbol_table, file_name)
             self.print_sucess("Análise Léxica concluída sem erros")
             try:
                 syntax = Syntax(tokens)
@@ -49,3 +52,21 @@ class Compiler:
     @staticmethod
     def print_sucess(mensagem):
         sys.stdout.write(f"\033[32m{mensagem}\033[0m\n")
+
+    @staticmethod
+    def save_tokens_to_csv(tokens, file_name):
+        with open(f'../resources/out/{file_name}_tokens.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Token', 'Lexema', 'Line'])  # Cabeçalho
+            for token_type, token_value, token_line in tokens:
+                writer.writerow([token_type, token_value, token_line])
+        print("Tokens salvos em 'tokens.csv'.")
+
+    @staticmethod
+    def save_symbol_table_to_csv(symbol_table, file_name):
+        with open(f'../resources/out/{file_name}_symbol_table.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['ID', 'Type', 'Line'])  # Cabeçalho
+            for symbol in symbol_table:
+                writer.writerow([symbol.id, symbol.kind, symbol.line])
+        print("Tabela de símbolos salva em 'symbol_table.csv'.")
--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from src.compiler import Compiler
 def main():
     compiler = Compiler()
     for i in range(1, 11):
-        file_path = f'../resources/test_lexer/code{i}_1.txt'
-        compiler.compile(file_path)
-        file_path = f'../resources/test_lexer/code{i}_2.txt'
-        compiler.compile(file_path)
+        file_name = f'code{i}_1.txt'
+        compiler.compile(file_name)
+        file_name = f'code{i}_2.txt'
+        compiler.compile(file_name)
 
 
 if __name__ == "__main__":
