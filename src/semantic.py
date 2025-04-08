5eef4130d0c328be9eb0bc9917a5c0e91f9f8fe6
leonardonb <leonardonb@gmail.com>
2025-04-07 23:47:27 -0300

    Alterando a ordem das funções na análise semântica

--- src/semantic.py
+++ src/semantic.py
@@ -19,22 +19,23 @@ class Semantic:
     def check_declarations(self):
         print("\n- Verificando declaração -")
 
-    # Confirma se os operandos têm tipos compatíveis nas expressões.
-    def check_expression_types(self):
-        print("\n- Verificando tipos em expressões -")
-
     # Verifica se o lado esquerdo da atribuição existe e tem tipo compatível com o lado direito.
     def check_assignments(self):
         print("\n- Verificando atribuições -")
+
+    # Confirma se os operandos têm tipos compatíveis nas expressões.
+    def check_expression_types(self):
+        print("\n- Verificando tipos em expressões -")
 
     # Confere se as chamadas usam o número e tipo corretos de argumentos.
     def check_function_calls(self):
         print("\n- Verificando chamadas de funlções -")
 
-    # Garante que as expressões condicionais em if e while são do tipo booleano.
-    def check_control_flow_conditions(self):
-        print("\n- Verificando instrução de controle de fluxo -")
-
     # Verifica se existe return com tipo compatível nas funções.
     def check_function_returns(self):
         print("\n- Verificando compatibilidade do returns -")
+
+   # Garante que as expressões condicionais em if e while são do tipo booleano.
+    def check_control_flow_conditions(self):
+        print("\n- Verificando instrução de controle de fluxo -")
+
