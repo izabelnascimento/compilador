6764070b0a3caf1b2c34465c0132ad9944dc76eb
Izabel Nascimento <izabelyale@hotmail.com>
2025-04-10 01:27:12 -0300

    merge TAC generator

--- src/main.py
+++ src/main.py
@@ -9,19 +9,21 @@ def execute_all_folder(folder, n):
     for i in range(1, n):
         file_name = f'code{i}_1'
         Compiler.compile(folder, file_name)
-        file_name = f'code{i}_2'
-        Compiler.compile(folder, file_name)
+        if folder != 'test_lexer':
+            file_name = f'code{i}_2'
+            Compiler.compile(folder, file_name)
 
 
 def execute_all():
     execute_all_folder('test_lexer', 11)
     execute_all_folder('test_syntax', 6)
-    execute_all_folder('test_semantic', 2)
+    execute_all_folder('test_semantic', 5)
+
 
 def main():
-    folder = 'test_semantic'
-    # execute_one(folder, f'code4_2')
-    execute_all_folder(folder, 5)
+    folder = 'test_tac_generator'
+    # execute_one(folder, f'code3_1')
+    execute_all_folder(folder, 4)
     # execute_all()
 
 
